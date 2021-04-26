64dff255ef25de4728dea399b40691d1ed8a2bd2
Eldar Klyuev <e.s.klyuev@gmail.com>
2021-04-26 18:47:07 +0300

    Change color first_vis

--- first_vis.py
+++ first_vis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import vtk
+import time
 
 from laspy.file import File
 inFile = File("Avrora_Part.las", mode = "r")
@@ -17,16 +18,23 @@ POINT_SIZE = 5
 
 # print(inFile.x[0],inFile.y[0],inFile.z[0])
 # Create the geometry of a point (the coordinate)
+# Используем для хранения информации о координатах точек
 points = vtk.vtkPoints()
 
 # Create the topology of the point (a vertex)
 vertices = vtk.vtkCellArray()
+
+# Set color points
+Colors = vtk.vtkUnsignedCharArray()
+Colors.SetNumberOfComponents(3)
+Colors.SetName("Colors")
 
 for k in range(POINT_COUNT):
     # id = points.InsertNextPoint(pnt[k])
     id = points.InsertNextPoint([inFile.x[k],inFile.y[k],inFile.z[k]])
     vertices.InsertNextCell(1)
     vertices.InsertCellPoint(id)
+    Colors.InsertNextTuple3(255, 0, 0)
 
 # Create a polydata object
 point = vtk.vtkPolyData()
@@ -34,6 +42,7 @@ point = vtk.vtkPolyData()
 # Set the points and vertices we created as the geometry and topology of the polydata
 point.SetPoints(points)
 point.SetVerts(vertices)
+point.GetPointData().SetScalars(Colors)
 
 # Visualize
 mapper = vtk.vtkPolyDataMapper()
@@ -55,17 +64,17 @@ camera.SetPosition(13.55115266673536, -29.350476103164837, -1.3432727339253447)
 camera.SetFocalPoint(43.55115266673536, -29.350476103164837, -1.3432727339253447)
 renderer.SetActiveCamera(camera)
 
-# camera.SetFocalPoint(0, 0, 0)
-
 renderWindow = vtk.vtkRenderWindow()
 renderWindow.AddRenderer(renderer)
 renderWindowInteractor = vtk.vtkRenderWindowInteractor()
 renderWindowInteractor.SetRenderWindow(renderWindow)
 renderer.AddActor(actor)
 
-# camera.SetViewUp(0, 0, 1)
 
 renderWindow.Render()
+# renderWindowInteractor.FlyTo(renderer, 55.55115266673536, -9.350476103164837, -1.3432727339253447)
 renderWindowInteractor.Start()
+
+
 
 
